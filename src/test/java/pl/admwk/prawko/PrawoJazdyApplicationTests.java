16cf8bd29b93f68ff9b00987c74c9c95f5507e57
MarekWlodarskii <wlodarski.marek96@gmail.com>
2024-06-24 21:12:57 +0200

    Backend

---
+++ src/test/java/pl/admwk/prawko/PrawoJazdyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.admwk.prawko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrawoJazdyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
